4165bac1430547119f8e983741ee0b7ed6ea3845
2843951 <2843951@JS101MAC-009.student.gcd.ie>
2013-04-10 13:11:02 +0100

    Initial Commit

---
+++ GitExample/src/ie/gcd/git/Sample.java
@@ -0,0 +1,20 @@
+package ie.gcd.git;
+
+public class Sample {
+
+	
+	public Sample(){
+		//TODO
+	
+	}
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
